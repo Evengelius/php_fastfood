eb48354b5fee2a3339191c6f8f9927c1143dc29f
Molimo Abel <molimoabel@gmail.com>
2020-07-13 00:10:11 +0200

    Update commit

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 define ('DSN', 'mysql:dbname=fastfood;host=localhost;charset=utf8');
-define ('USER', 'root');
-define ('PASSWORD','root');
+define ('USER', '');
+define ('PASSWORD','');
 define ("VAT",21);
 define ("MAX_BURGER",2);
 define ("MAX_DRINK",2);
